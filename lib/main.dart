f2c305b9bc811d1477aaab0051a3eaa15280580e
najeebaslan <najeebaslan2019@gmail.com>
2023-02-24 00:56:05 +0300

    Context Extension Branch

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,9 @@
-import 'dart:convert';
+import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_utils_project/flutter_utils_project.dart';
-import 'package:http/http.dart' as http;
-
-import 'model/posts.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
@@ -17,111 +13,73 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Get data from api',
+      title: 'Flutter Extensions',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(),
+      home: const ContextExtension(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-  Future<List<Posts>> getData() async {
-    try {
-      const String uri = 'https://jsonplaceholder.typicode.com/photos';
-
-      final response = await http.get(Uri.parse(uri));
-      if (response.statusCode == 200) {
-        List post = jsonDecode(response.body);
-
-        final result = post.map((json) => Posts.fromJson(json)).toList();
-
-        return result;
-      } else {
-        throw Exception('Failed to load data from api');
-      }
-    } catch (e) {
-      rethrow;
-    }
-  }
+class ContextExtension extends StatelessWidget {
+  const ContextExtension({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('get data form api'),
-        centerTitle: true,
+        title: const Text('Context Extensions'),
       ),
-      body: SnapHelperWidget(
-          future: getData(),
-          loadingWidget: const Center(child: Text('waiting data')),
-          onSuccess: (List<Posts> onSuccess) => shaowData(onSuccess)),
-      // FutureBuilder(
-      //   future: getData(),
-      //   builder: (context, snapshot) {
-      //     //Error
-      //     if (snapshot.hasError) return Text(snapshot.error.toString());
-      //     //Success
-      //     if (snapshot.hasData) {
-      //       return shaowData(snapshot.data!);
-      //     } else {
-      //       //Loading
-      //       return const Center(child: CircularProgressIndicator());
-      //     }
-      //   },
-      // ),
+      body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        //? ============== Context Extensions ==================//
+
+        //! 1- height - width
+        Container(
+          color: Colors.amberAccent,
+          height: context.height / 2.1,
+          width: context.width,
+          // height: MediaQuery.of(context).size.height / 2.1,
+          // width: MediaQuery.of(context).size.width,
+        ),
+
+        //! 2- push Navigator
+        // ElevatedButton(
+        //     onPressed: () =>
+        //     context.push(const Page2()),
+        //     //  Navigator.of(context)
+        //     // .push(MaterialPageRoute(builder: (context) => const Page2())),
+        //     child: const Text('Navigator')),
+
+        //! 3- textTheme
+        // Text(
+        //   'text Theme',
+        //   style:
+        //   context.textTheme.headline6
+        // // Theme.of(context).textTheme.headline6,
+        // ),
+
+        //! 4- isDarkMode
+        ElevatedButton(
+            onPressed: () => log(context.isDarkMode.toString()),
+            child: const Text('isDarkMode')),
+      ]).center(),
     );
   }
 }
 
-Widget shaowData(List<Posts> data) {
-  return ListView.builder(
-    itemCount: data.length,
-    itemBuilder: (context, index) {
-      Posts myData = data[index];
-      return Container(
-        margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-        padding: const EdgeInsets.all(5),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          color: Colors.primaries[index % Colors.primaries.length]
-              .withOpacity(0.7),
-        ),
-        child: Row(children: [
-          ClipOval(
-            child: Image.network(
-              myData.thumbnailUrl,
-              height: 80,
-              width: 80,
-            ),
-          ),
-          Expanded(
-            child: Text(
-              myData.title,
-              style: const TextStyle(
-                  color: Colors.black,
-                  fontSize: 18,
-                  fontWeight: FontWeight.w700),
-            ),
-          ),
+class Page2 extends StatelessWidget {
+  const Page2({super.key});
 
-        //
-//flutter_utils_project_1
-          CircleAvatar(
-            radius: 15,
-            child: Text(
-              myData.id.toString(),
-              style: const TextStyle(
-                  color: Colors.black,
-                  fontSize: 18,
-                  fontWeight: FontWeight.w700),
-            ),
+  @override
+  Widget build(BuildContext context) => const Scaffold(
+        backgroundColor: Colors.brown,
+        body: Center(
+          child: Text(
+            'Page2',
+            style: TextStyle(color: Colors.white),
           ),
-        ]),
+        ),
       );
-    },
-  );
 }
